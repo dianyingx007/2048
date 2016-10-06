791bd500a2de422d7973b889e8602b286acf76ee
dianyingx007 <dianyingx007@163.com>
2016-10-06 13:02:03 +0800

    v1.4 增加了移动端事件 日常修复bug

--- js/2048.js
+++ js/2048.js
@@ -348,5 +348,47 @@ taskClass.prototype.clear=function(){
 //button点击重新开始
 var restart=document.getElementById("restart");
 restart.onclick = function(){
+	event.stopPropagation();
 	game.start();
 };
+restart.addEventListener("touchstart",function(){
+	event.stopPropagation();
+	game.start();
+},false);
+
+//移动端控制事件
+var init = {minx:20,miny:20,sx:0,sy:0,ex:0,ey:0};//minx,miny限定多少才算移动
+document.addEventListener("touchstart",function(){
+	var event = window.event || arguments[0];
+	init.sx=event.targetTouches[0].pageX;
+	init.sy=event.targetTouches[0].pageY;
+	init.ex=init.sx;
+	init.ey=init.sy;
+},false);
+document.addEventListener("touchmove",function(){
+	var event = window.event || arguments[0];
+	event.preventDefault();//阻止浏览器滚动条滚动、缩放
+	init.ex=event.targetTouches[0].pageX;
+	init.ey=event.targetTouches[0].pageY;
+},false);
+document.addEventListener("touchend",function(){
+	if(game.state!=2){
+		if(game.state===1){
+			var changeX = init.ex - init.sx;
+			var changeY = init.ey - init.sy;
+			if(Math.abs(changeX)>Math.abs(changeY)&&Math.abs(changeX)>init.minx){
+				if(changeX>0){
+					game.moveRight();
+				}else {
+					game.moveLeft();
+				}
+			}else if(Math.abs(changeY)>Math.abs(changeX)&&Math.abs(changeY)>init.miny){
+				if(changeY>0){
+					game.moveBottom();
+				}else {
+					game.moveTop();
+				}
+			}
+		}
+	}
+},false);
